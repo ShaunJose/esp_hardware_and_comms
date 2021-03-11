dfb821a32833146b33db271dade8857150802e5a
ShaunJose <shaun.jose12@gmail.com>
2021-03-11 21:03:55 +0000

    Motion sensor code

--- blink_led.py
+++ blink_led.py
@@ -3,16 +3,33 @@ print('blink_led.py is loaded!')
 import time
 from machine import Pin
 
-led_pin = 2
 
-def run(pin=led_pin):
+def run():
 
-    p = Pin(pin,Pin.OUT)
+    led_pin = 2
+    pir_pin = 0
+    power_pin = 5
+    curr_state = 0
+
+    time.sleep(4)
+
+    # Define pins and set init values
+    led = Pin(led_pin, Pin.OUT)
+    pir = Pin(pir_pin, Pin.IN)
+    power = Pin(power_pin, Pin.OUT)
+    led.value(0)
+    pir.value(0)
+    power.value(1)
+
+    time.sleep(4)
 
     while True:
-        print("powering pin on")
-        p.value(1)
-        time.sleep(1)
-        print("powering pin off")
-        p.value(0)
-        time.sleep(1)
+        if pir.value() == 1 and curr_state == 0:
+            print("Motion detected!")
+            led.value(1)
+            curr_state = 1
+        if pir.value() == 0 and curr_state == 1:
+            print("Motion stopped!")
+            led.value(0)
+            curr_state = 0
+            time.sleep(5)
