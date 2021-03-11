2fe00ebaac2de35177f282e89bdb2c4f2192e0fc
ShaunJose <shaun.jose12@gmail.com>
2021-03-11 21:11:51 +0000

    Small changes

--- blink_led.py
+++ blink_led.py
@@ -11,25 +11,25 @@ def run():
     power_pin = 5
     curr_state = 0
 
-    time.sleep(4)
+    time.sleep(4) # Wait so program doesnt complain that vars arent declared
 
     # Define pins and set init values
     led = Pin(led_pin, Pin.OUT)
     pir = Pin(pir_pin, Pin.IN)
-    power = Pin(power_pin, Pin.OUT)
-    led.value(0)
-    pir.value(0)
+    power = Pin(power_pin)
+    led.off()
+    pir.off()
     power.value(1)
 
-    time.sleep(4)
+    time.sleep(4) # Wait so program doesnt complain that pins arent set
 
     while True:
         if pir.value() == 1 and curr_state == 0:
             print("Motion detected!")
-            led.value(1)
+            led.on()
             curr_state = 1
         if pir.value() == 0 and curr_state == 1:
             print("Motion stopped!")
-            led.value(0)
+            led.off()
             curr_state = 0
             time.sleep(5)
