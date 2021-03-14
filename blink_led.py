7c1bd2e645cf3e1db84938d367a97aa87a8992b7
ShaunJose <shaun.jose12@gmail.com>
2021-03-14 15:32:26 +0000

    Make blink_led a lot cleaner

--- blink_led.py
+++ blink_led.py
@@ -1,34 +1,28 @@
-print('blink_led.py is loaded!')
-
 import time
 from machine import Pin
 
+print('blink_led.py is loaded!')
 
 def run():
 
+    # Define pin numbers
     led_pin = 33
     pir_pin = 25
     curr_state = 0
 
-    time.sleep(4) # Wait so program doesnt complain that vars arent declared
-
-    # Define pins and set init values
+    # Define pins and turn everything off
     led = Pin(led_pin, Pin.OUT)
     pir = Pin(pir_pin, Pin.IN)
     led.off()
     pir.off()
 
-    time.sleep(4) # Wait so program doesnt complain that pins arent set
-
-    time.sleep(60)
-
+    # Turn led on when motion detected and off when it's stopped
     while True:
-        if curr_state == 1 and pir.value() == 0:
-            print("Motion stopped!")
-            led.off()
-            curr_state = 0
-            time.sleep(5)
         if curr_state == 0 and pir.value() == 1:
             print("Motion detected!")
             led.on()
             curr_state = 1
+        if curr_state == 1 and pir.value() == 0:
+            print("Motion stopped!")
+            led.off()
+            curr_state = 0
