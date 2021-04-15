b187ca21ffc17e2d023aeff644ae38723dfd5f2d
ShaunJose <shaun.jose12@gmail.com>
2021-04-16 00:44:21 +0100

    Delete redundant files
    Removed micrpython and singular code files

--- boot.py
+++
@@ -1,7 +0,0 @@
-# Credit: ClaytonDarwin
-# Source: https://drive.google.com/file/d/1VTd666oFIcNdoo9qhNknqRkn59JdU7g5/view
-
-# This file is executed on every boot (including wake-boot from deepsleep)
-
-# notify
-print('RUN: boot.py')
